5462fc5cf526734fa76e3f5af77e7b603438b037
uniS2 <s2oie1010@gmail.com>
2023-10-30 18:20:30 +0900

    [style] : 등록 페이지 스타일 작업 완료

--- 01.javascript/todoapp/assets/js/pages/list/TodoList.js
+++ 01.javascript/todoapp/assets/js/pages/list/TodoList.js
@@ -4,14 +4,14 @@ import Footer from '../../layout/Footer.js';
 import TodoRegist from '../regist/TodoRegist.js';
 import TodoInfo from '../info/TodoInfo.js';
 
-const TodoList = async function(){
+const TodoList = async function () {
   const page = document.createElement('div');
   page.setAttribute('id', 'page');
-  
+
   const content = document.createElement('div');
   content.setAttribute('id', 'content');
   let response;
-  try{
+  try {
     response = await axios('http://localhost:33088/api/todolist');
 
     const ul = document.createElement('ul');
@@ -22,10 +22,10 @@ const TodoList = async function(){
       todoInfoLink.setAttribute('href', `info?_id=${item._id}`);
       const title = document.createTextNode(item.title);
 
-      todoInfoLink.addEventListener('click', async function(event){
+      todoInfoLink.addEventListener('click', async function (event) {
         // 브라우저의 기본 동작 취소(<a> 태그 동작 안하도록)
         event.preventDefault();
-        const infoPage = await TodoInfo({_id: item._id});
+        const infoPage = await TodoInfo({ _id: item._id });
         document.querySelector('#page').replaceWith(infoPage);
       });
 
@@ -45,11 +45,11 @@ const TodoList = async function(){
       document.querySelector('#page').replaceWith(registPage);
     });
 
-  }catch(err){
+  } catch (err) {
     const error = document.createTextNode('일시적인 오류 발생');
     content.appendChild(error);
   }
-  
+
   page.appendChild(Header('TODO App 목록 조회'));
   page.appendChild(content);
   page.appendChild(Footer());
--- 01.javascript/todoapp/assets/js/pages/regist/TodoRegist.js
+++ 01.javascript/todoapp/assets/js/pages/regist/TodoRegist.js
@@ -2,16 +2,37 @@
 import Header from '../../layout/Header.js';
 import Footer from '../../layout/Footer.js';
 
-const TodoRegist = function(){
+const TodoRegist = function () {
   const page = document.createElement('div');
-  page.setAttribute('id', 'page');
+  page.setAttribute('id', 'detailPage');
 
-  const content = document.createElement('div');
-  const text = document.createTextNode('등록 화면');
-  content.appendChild(text);
+  const detailPage = document.createElement('form');
+  page.setAttribute('id', 'detailForm');
+  page.appendChild(detailPage);
+
+  const title = document.createElement('input');
+  const titleContent = document.createTextNode('할 일');
+  detailPage.appendChild(title);
+  title.appendChild(titleContent);
+
+  const detail = document.createElement('textarea');
+  const detailContent = document.createTextNode('상세 내용을 입력하세요.');
+  detailPage.appendChild(detail);
+  detail.appendChild(detailContent);
+
+  const btnAdd = document.createElement('button');
+  const btnAddTodo = document.createTextNode('등록');
+  btnAdd.setAttribute('type', 'submit');
+  btnAdd.appendChild(btnAddTodo);
+  detailPage.appendChild(btnAdd);
+
+  const btnCancle = document.createElement('button');
+  const btnCancleTodo = document.createTextNode('취소');
+  btnCancle.appendChild(btnCancleTodo);
+  detailPage.appendChild(btnCancle);
 
   page.appendChild(Header('TODO App 등록'));
-  page.appendChild(content);
+  page.appendChild(detailPage);
   page.appendChild(Footer());
 
   return page;
