5bf5323d710031262d86f69527dbb359ab223dc2
jungmyungjin <myung2667@gmail.com>
2023-10-31 14:22:56 +0900

    [feat] : 상세조회, 토글 UI 완성

--- 01.javascript/todoapp/assets/js/pages/list/TodoList.js
+++ 01.javascript/todoapp/assets/js/pages/list/TodoList.js
@@ -2,7 +2,40 @@
 import Header from "../../layout/Header.js";
 import Footer from "../../layout/Footer.js";
 import TodoRegist from "../regist/TodoRegist.js";
-import TodoInfo from "../info/TodoInfo.js";
+// import TodoInfo from "../info/TodoInfo.js";
+import { linkTo } from "../../Router.js";
+
+const toggleDetailTodo = function (title, content) {
+  const detailTodo = document.createElement("form");
+  detailTodo.setAttribute("id", "detailTodo");
+
+  const detailTitle = document.createElement("input");
+  detailTitle.value = title;
+  detailTitle.setAttribute("id", "detailTitle");
+
+  const detailContent = document.createElement("textarea");
+  detailContent.textContent = content;
+  detailContent.setAttribute("id", "detailContent");
+
+  const btnDetailWrapper = document.createElement("div");
+  btnDetailWrapper.setAttribute("class", "divWrapper");
+
+  const btnDetailEdit = document.createElement("button");
+  btnDetailEdit.textContent = "수정";
+  btnDetailEdit.setAttribute("id", "btnAdd");
+
+  const btnDetailDelete = document.createElement("button");
+  btnDetailDelete.textContent = "삭제";
+  btnDetailDelete.setAttribute("id", "btnCancle");
+
+  detailTodo.appendChild(detailTitle);
+  detailTodo.appendChild(detailContent);
+  detailTodo.appendChild(btnDetailWrapper);
+  btnDetailWrapper.appendChild(btnDetailEdit);
+  btnDetailWrapper.appendChild(btnDetailDelete);
+
+  return detailTodo;
+};
 
 const TodoList = async function () {
   // 전체 div 박스
@@ -19,9 +52,13 @@ const TodoList = async function () {
     // ul
     const ul = document.createElement("ul");
     ul.setAttribute("class", "todolist");
-    response.data?.items.forEach((item) => {
+    response.data?.items.forEach(async (item) => {
+      //test
+      console.log(item);
+
       // li
       const li = document.createElement("li");
+      const todoContent = document.createElement("div");
       const checkbox = document.createElement("input");
       const title = document.createElement("div");
       title.setAttribute("class", "title");
@@ -31,29 +68,85 @@ const TodoList = async function () {
       li.setAttribute("class", "list");
       const text = document.createTextNode(item.title);
 
-      const todoInfoLink = document.createElement("a");
+      // const todoInfoLink = document.createElement("a");
       // todoInfoLink.setAttribute("href", `info?_id=${item._id}`);
       // const title = document.createTextNode(item.title);
 
-      // todoInfoLink.addEventListener("click", async function (event) {
-      //   // 브라우저의 기본 동작 취소(<a> 태그 동작 안하도록)
-      //   event.preventDefault();
-      //   const infoPage = await TodoInfo({ _id: item._id });
-      //   document.querySelector("#page").replaceWith(infoPage);
-      // });
+      /*
+      todoInfoLink.addEventListener("click", async function (event) {
+        // 브라우저의 기본 동작 취소(<a> 태그 동작 안하도록)
+        event.preventDefault();
+        const infoPage = await TodoInfo({ _id: item._id });
+        document.querySelector("#page").replaceWith(infoPage);
+      });
+      */
 
       title.appendChild(text);
 
-      // todoInfoLink.appendChild(title);
+      let showToggle = true;
+      li.addEventListener("click", async function (event) {
+        // 브라우저의 기본 동작 취소(<a> 태그 동작 안하도록)
+
+        const detailResponse = await axios(
+          `http://localhost:33088/api/todolist/${item._id}`
+        );
+        const { title, content } = detailResponse.data?.item;
+        const openToggleDetail = toggleDetailTodo(title, content);
+        if (showToggle) {
+          event.preventDefault();
+
+          todoContent.appendChild(openToggleDetail);
+        } else {
+          todoContent.removeChild(todoContent.lastChild);
+        }
+        showToggle = !showToggle;
+      });
+
       li.appendChild(checkbox);
-      li.appendChild(title);
-      // li.appendChild(todoInfoLink);
+      li.appendChild(todoContent);
+      todoContent.appendChild(title);
+      // if (showToggle) {
+      //   console.log(item);
+      //   const detailResponse = await axios(
+      //     `http://localhost:33088/api/todolist/${item._id}`
+      //   );
+      //   const { title, content } = detailResponse.data?.item;
+      //   console.log(detailResponse);
+
+      //   const openToggleDetail = toggleDetailTodo(title, content);
+      //   li.appendChild(openToggleDetail);
+      // } else {
+      //   li.appendChild(checkbox);
+      //   li.appendChild(title);
+      // }
+
+      ///////////
+      // li.addEventListener("click", async function (event) {
+      //   // 브라우저의 기본 동작 취소(<a> 태그 동작 안하도록)
+      //   event.preventDefault();
+      //   const detailResponse = await axios(
+      //     `http://localhost:33088/api/todolist/${item._id}`
+      //   );
+      //   const { title, content } = detailResponse.data?.item;
+      //   console.log(detailResponse);
+
+      //   const openToggleDetail = toggleDetailTodo(title, content);
+      //   li.appendChild(openToggleDetail);
+      // });
       ul.appendChild(li);
     });
     content.appendChild(ul);
 
-    const btnTitle = document.createTextNode("등록");
+    // const btnTitle = document.createTextNode("등록");
     const btnRegist = document.createElement("button");
+    const btnTitle = document.createTextNode("등록");
+    btnRegist.setAttribute("class", "enrollBtn");
+    btnRegist.appendChild(btnTitle);
+    content.appendChild(btnRegist);
+
+    btnRegist.addEventListener("click", () => {
+      linkTo("regist");
+    });
     btnRegist.setAttribute("class", "enrollBtn");
     btnRegist.appendChild(btnTitle);
     content.appendChild(btnRegist);
