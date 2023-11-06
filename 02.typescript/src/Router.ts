6ee655d652bc76817ff0dc2bf93d842585426a03
uniS2 <s2oie1010@gmail.com>
2023-11-06 10:31:02 +0900

    init: TypeScript 프로젝트 구성

---
+++ 02.typescript/src/Router.ts
@@ -0,0 +1,42 @@
+import TodoList from "./pages/list/TodoList";
+import TodoRegist from "./pages/regist/TodoRegist";
+import TodoInfo from "./pages/info/TodoInfo";
+import Error404 from "./pages/errors/Error404";
+
+async function getPage() {
+  let page;
+  console.log(location);
+  switch (location.pathname) {
+    case "/":
+      page = await TodoList();
+      break;
+    case "/regist":
+      page = TodoRegist();
+      break;
+    case "/info":
+      page = await TodoInfo();
+      break;
+    default:
+      page = Error404();
+  }
+
+  return page;
+}
+
+async function render() {
+  const page = await getPage();
+  document.querySelector("#page")!.replaceWith(page);
+}
+
+window.addEventListener("popstate", render);
+
+export function linkTo(url: string) {
+  history.pushState({}, "todo", url);
+  render();
+}
+
+const Router = async function () {
+  return await getPage();
+};
+
+export default Router;
