9506621338db8c2e03fe547f4e2a94b104596105
NAYEONG OH <102240503+ony540@users.noreply.github.com>
2023-11-10 17:56:48 +0900

    style : 메인페이지 스타일링 추가

--- 03.react/src/component/common/Header.tsx
+++ 03.react/src/component/common/Header.tsx
@@ -3,17 +3,19 @@ import styled from 'styled-components';
 
 export const Header = () => {
   const location = useLocation();
+
   return (
-    <StyledHeader>
+    <StyledHeader isregist={location.pathname === '/regist'}>
       <h1>{location.pathname === '/regist' ? 'Make Your Plan' : 'What to do Today'}</h1>
     </StyledHeader>
   );
 };
 
-const StyledHeader = styled.div`
+const StyledHeader = styled.div<{ isregist: boolean }>`
+  background-color: ${(p) => p.isregist && '#d9d9d9'};
   text-align: center;
-  background-color: #d9d9d9;
   margin: 0 auto;
+
   width: 360px;
   border-top-left-radius: 5px;
   border-top-right-radius: 5px;
--- 03.react/src/pages/mainPage/TodoListMain.tsx
+++ 03.react/src/pages/mainPage/TodoListMain.tsx
@@ -2,6 +2,7 @@ import TodoUpdate from '../../component/updatePage/TodoUpdate';
 import { getTodoList, deleteTodoItem, patchTodoItem } from '../../API/axios';
 import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import styled from 'styled-components';
 
 export const TodoListMain = () => {
   const [data, setData] = useState<TodoItem[]>();
@@ -47,17 +48,82 @@ export const TodoListMain = () => {
 
   return (
     <>
-      {data?.map((item) => (
-        <li key={item._id}>
-          <input type='checkbox' onChange={() => checkTodoDone(item)} checked={item.done} />
-          <div onClick={() => showTodoDetail(item._id)}>{item.title}</div>
-          <TodoUpdate idNum={item._id} toggle={toggle[item._id]} getData={getData} />
-        </li>
-      ))}
-      <div>
-        <button onClick={moveToRegist}>등록</button>
-        <button onClick={deleteAllTodo}>전체삭제</button>
-      </div>
+      <TodoList>
+        {data?.map((item) => (
+          <li key={item._id}>
+            <div>
+              <input type='checkbox' onChange={() => checkTodoDone(item)} checked={item.done} />
+              <TodoTitle onClick={() => showTodoDetail(item._id)}>{item.title}</TodoTitle>
+            </div>
+
+            <TodoUpdate idNum={item._id} toggle={toggle[item._id]} getData={getData} />
+          </li>
+        ))}
+      </TodoList>
+      <ButtonContainer>
+        <Button onClick={moveToRegist}>등록</Button>
+        <Button className='redButton' onClick={deleteAllTodo}>
+          전체삭제
+        </Button>
+      </ButtonContainer>
     </>
   );
 };
+
+const TodoList = styled.ul`
+  width: 300px;
+  list-style: none;
+  padding: 0;
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+  margin: 0 auto 20px;
+
+  li div {
+    display: flex;
+  }
+
+  & > input {
+    display: inline-block;
+    margin-right: 10px;
+    width: 20px;
+  }
+`;
+
+const TodoTitle = styled.h2`
+  display: inline-block;
+  background-color: #d9d9d9;
+  width: 260px;
+  height: 40px;
+  font-size: 16px;
+  border-radius: 75px;
+  color: black;
+  text-align: left;
+  justify-content: start;
+  padding-left: 30px;
+  display: flex;
+  align-items: center;
+  cursor: pointer;
+`;
+
+// BUTTON
+const ButtonContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  margin-bottom: 60px;
+`;
+
+const Button = styled.button`
+  border: none;
+  border-radius: 20px;
+  background-color: #79e127;
+  color: #fff;
+  padding: 6px 12px;
+  flex-shrink: 0;
+  cursor: pointer;
+
+  &.redButton {
+    margin-left: 20px;
+    background-color: #ef5242;
+  }
+`;
